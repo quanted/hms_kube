bf2b2ac6469a5de2130cef6c610f41611dba135e
Deron Smith <deron.brock@gmail.com>
2022-08-03 09:06:50 -0400

    Update hms-volume-setup.sh

--- hms-volume-setup.sh
+++ hms-volume-setup.sh
@@ -9,6 +9,7 @@ kubectl exec deploy/oms-shell -- /bin/bash -c "aws s3 cp s3://qed-stg/hms /tmp/h
 kubectl exec deploy/oms-shell -- /bin/bash -c "cd /mnt/hms-dotnetcore-appdata; rm -r *"
 kubectl exec deploy/oms-shell -- /bin/bash -c "cp -rf /tmp/hms/* /mnt/hms-dotnetcore-appdata"
 kubectl exec deploy/oms-shell -- /bin/bash -c "rm -rf /tmp/hms/"
+kubectl exec deploy/oms-shell -- /bin/bash -c "mkdir /mnt/mongodb-pvolume/hms/"
 
 echo "Completed updates from s3 bucket"
 echo "Completed QED code update"
